b241e7fc10050cf78b9a6ce903d80d36ffa993a5
saegyeol <yoonsaegyeol@gmail.com>
2020-12-31 19:40:29 +0900

    뷰 컨트롤러 초기화 방법 수정 적용

--- SearchBook/SceneDelegate.swift
+++ SearchBook/SceneDelegate.swift
@@ -18,8 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
     guard let _ = (scene as? UIWindowScene) else { return }
     let apiService = APIService(client: APIClient.shared)
-    let searchViewController = SearchViewController()
-    searchViewController.apiService = apiService
+    let viewModel = SearchViewModel(apiService: apiService)
+    let searchViewController = SearchViewController(viewModel: viewModel)
     let navigationViewController = NavigationController(rootViewController: searchViewController)
     self.window?.rootViewController = navigationViewController
     self.window?.makeKeyAndVisible()
