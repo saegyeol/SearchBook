2087a4642de9499ef1ef75acc9fb4468fb35008f
saegyeol <yoonsaegyeol@gmail.com>
2020-12-31 19:39:23 +0900

    뷰 컨트롤러 수정

---
+++ SearchBook/Source/ViewControllers/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  SearchBook
+//
+//  Created by 윤새결 on 2020/12/31.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+  
+  
+  
+  
+  
+  
+  
+}
---
+++ SearchBook/Source/ViewControllers/SearchViewController.swift
@@ -0,0 +1,173 @@
+//
+//  SearchViewController.swift
+//  SearchBook
+//
+//  Created by 윤새결 on 2020/12/28.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+  
+  private let viewModel: SearchViewModelType
+  private var lastQuery: String = ""
+  private var recentQueries: [String] = UserDefaults.standard.array(forKey: "recentQuery") as? [String] ?? []
+  
+  private var isSearchBarTextEmpty: Bool {
+    return self.searchController.searchBar.text?.isEmpty ?? true
+  }
+  private var isSearching: Bool {
+    return self.searchController.isActive && !self.isSearchBarTextEmpty && self.viewModel.simpleBooks.isEmpty
+  }
+  private var isSearchEnd: Bool {
+    return !self.isSearching && !self.viewModel.simpleBooks.isEmpty
+  }
+  
+  private let tableView: UITableView = UITableView()
+  private let searchController = UISearchController(searchResultsController: nil)
+  
+  init(viewModel: SearchViewModelType) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func loadView() {
+    super.loadView()
+    
+    self.title = "Search"
+    
+    //tableView setting
+    self.tableView.delegate = self
+    self.tableView.dataSource = self
+    self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    self.view.addSubview(self.tableView)
+    
+    //searchController setting
+    self.searchController.hidesNavigationBarDuringPresentation = true
+    self.searchController.obscuresBackgroundDuringPresentation = false
+    self.searchController.searchBar.delegate = self
+    self.searchController.searchBar.placeholder = "Search Books"
+    self.searchController.searchResultsUpdater = self
+    self.navigationItem.hidesSearchBarWhenScrolling = false
+    self.navigationItem.searchController = self.searchController
+  }
+  
+  //MARK:- Life Cycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  
+    //tableView autolayout
+    self.tableView.translatesAutoresizingMaskIntoConstraints = false
+    let tableViewConstraint = [
+      self.tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+      self.tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+      self.tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+      self.tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+    ]
+    NSLayoutConstraint.activate(tableViewConstraint)
+  }
+  
+  private func searchBooks(query: String) {
+    print("search start query: \(query)")
+    self.viewModel.search(with: query) { [weak self] in
+      guard let self = self else { return }
+      self.tableView.reloadData()
+    } failHandler: {
+      
+    }
+  }
+}
+
+extension SearchViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if self.isSearchBarTextEmpty || self.isSearching {
+      let query = self.recentQueries[indexPath.row]
+      self.searchController.searchBar.text = query
+      self.searchController.isActive = true
+      self.searchBarSearchButtonClicked(self.searchController.searchBar)
+      self.searchController.searchBar.resignFirstResponder()
+      
+      return
+    }
+    print("디테일로 이동!!")
+    self.viewModel.moveToDetail(with: indexPath.row) {
+      
+    } failHandler: {
+      
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    let booksCount = self.viewModel.simpleBooks.count
+    if booksCount != 0, indexPath.row >= booksCount - 5 {
+      guard let query = self.searchController.searchBar.text else { return }
+      self.searchBooks(query: query)
+    }
+  }
+}
+
+extension SearchViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if self.isSearchBarTextEmpty {
+      return self.recentQueries.count
+    }
+    return self.viewModel.simpleBooks.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+    
+    if self.isSearchBarTextEmpty {
+      let recentQuery = self.recentQueries[indexPath.row]
+      cell.textLabel!.text = recentQuery
+      
+      return cell
+    }
+    
+    if self.isSearchEnd {
+      let book = self.viewModel.simpleBooks[indexPath.row]
+      cell.textLabel!.text = book.title
+      
+      return cell
+    }
+    
+    return cell
+  }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    
+  }
+  
+
+}
+
+extension SearchViewController: UISearchBarDelegate {
+  func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    DispatchQueue.main.async { [weak self] in
+      guard let self = self else {
+        return
+      }
+      self.tableView.reloadData()
+    }
+  }
+  
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    guard let query = searchBar.text else { return }
+    self.lastQuery = query
+    self.viewModel.reset { [weak self] in
+      guard let self = self else { return }
+      self.tableView.reloadData()
+    }
+    if !self.recentQueries.contains(query) {
+      self.recentQueries.append(query)
+      UserDefaults.standard.set(self.recentQueries, forKey: "recentQuery")
+    }
+    self.searchBooks(query: query)
+  }
+}
